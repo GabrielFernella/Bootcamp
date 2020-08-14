5dcbdeb1f00acdc1acdfa5a2f970cca30d8cae0c
GabrielFernella <GabrielFernella@users.noreply.github.com>
2020-08-14 01:09:20 -0300

    Learn Routes

--- IniciandoEmNode/src/index.js
+++ IniciandoEmNode/src/index.js
@@ -2,8 +2,35 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request,response)=>{
-  return response.json({ message: "Hello"})
+app.use(express.json())
+
+app.get('/projects', (request,response)=>{
+  return response.json([
+    'projects 1'
+  ])
+})
+
+app.post('/projects', (request,response)=>{
+  return response.json([
+    'projects 1',
+    'projects 2',
+    'projects 3'
+  ])
+})
+
+app.put('/projects/:id', (request,response)=>{
+  return response.json([
+    'projects 30',
+    'projects 20',
+    'projects 10'
+  ])
+})
+
+app.delete('/projects/:id', (request,response)=>{
+  return response.json([
+    'projects 30',
+    'projects 20',
+  ])
 })
 
 app.listen(3333, ()=>{
