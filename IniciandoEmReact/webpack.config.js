d6322f9d65da226acd65267e7c51eb7528c0e128
GabrielFernella <GabrielFernella@users.noreply.github.com>
2020-08-14 18:41:51 -0300

    updated

---
+++ IniciandoEmReact/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'), //utilizamos esse tipo de caminho para facilitar a leitura em diferentes sistemas
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //todos terminados em js
+        exclude: /node_modules/, //excluindo do babel
+        use: {
+          loader: 'babel-loader' //ferramenta para converter os arquivos js
+        }
+      }
+    ]
+  }
+}
