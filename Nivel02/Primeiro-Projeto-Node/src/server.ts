2f8ad874692620298cb4146be0936e6ad2b3e46e
GabrielDEV <gabrielfernella07@hotmail.com>
2020-11-07 11:35:23 -0300

    Tratativa de Erros

--- Nivel02/Primeiro-Projeto-Node/src/server.ts
+++ Nivel02/Primeiro-Projeto-Node/src/server.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata'; // precisa desse recurso do TypeORM
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
 import routes from './routes';
 
 import uploadConfig from './config/upload';
@@ -11,6 +14,24 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+// As tratativas de erros devem ser declaradas depois das rotas
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  // se o erro veio da tratativa de erros da minha aplicação
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server Error: ${err.message}`,
+  });
+});
 
 app.listen(3333, () => {
   console.log('\x1b[32m', 'Server started on http://localhost:3333 ✔');
--- Nivel02/Primeiro-Projeto-Node/src/services/CreateAppointmentService.ts
+++ Nivel02/Primeiro-Projeto-Node/src/services/CreateAppointmentService.ts
@@ -1,6 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentRepository';
 
@@ -27,7 +29,7 @@ class CreateAppointmentService {
       appointmentDate,
     );
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
