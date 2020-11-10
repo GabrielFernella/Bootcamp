7fed7f531feb79c53e1c94941790d142e0289a70
Gabriel Fernella <39594204+GabrielFernella@users.noreply.github.com>
2020-11-10 12:22:19 -0300

    layout Dashboard

--- Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/39594204?s=460&u=c3395abbd7a4c831add26f21eae655454fe34b2f&v=4"
+            alt="Fernella"
+          />
+          <div>
+            <strong>GabrielFernella/unform</strong>
+            <p>Criando aplicações react com typescript🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/39594204?s=460&u=c3395abbd7a4c831add26f21eae655454fe34b2f&v=4"
+            alt="Fernella"
+          />
+          <div>
+            <strong>GabrielFernella/unform</strong>
+            <p>Criando aplicações react com typescript🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/39594204?s=460&u=c3395abbd7a4c831add26f21eae655454fe34b2f&v=4"
+            alt="Fernella"
+          />
+          <div>
+            <strong>GabrielFernella/unform</strong>
+            <p>Criando aplicações react com typescript🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- Nivel03/05-primeiro-projeto-react/steps.md
+++ Nivel03/05-primeiro-projeto-react/steps.md
@@ -3,9 +3,11 @@
 1. npx create-react-app my-app --template typescript
 2. Config ESlint
 3. yarn add react-router-dom && yarn add @types/react-router-dom -D (Trabalhar com rotas na aplicação)
-4. yarn add styled-components (Ferramenta de perfumaria para React (trabalhar com css no JS))
+4. yarn add styled-components && yarn add @types/styled-components -D (Ferramenta de perfumaria para React (trabalhar com css no JS))
+5. yarn add polished (biblioteca que permite trabalhar com cores)
+6. yarn add react-icons (biblioteca de icones)
 
-5.
+7.
 
 ## process
 
