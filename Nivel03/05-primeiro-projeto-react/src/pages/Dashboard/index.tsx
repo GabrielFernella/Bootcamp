fbc2adeb29b77dc484aace3453979af95ebcbeb1
Gabriel Fernella <39594204+GabrielFernella@users.noreply.github.com>
2020-11-10 14:34:42 -0300

    localStorage repositories

--- Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,8 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
-
 import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
@@ -18,7 +17,22 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
