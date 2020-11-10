2ed28a8ff5e2979e2b3bb43f595cc6e31150644d
Gabriel Fernella <39594204+GabrielFernella@users.noreply.github.com>
2020-11-10 15:09:09 -0300

    Navigate Link

--- Nivel03/05-primeiro-projeto-react/src/routes/index.tsx
+++ Nivel03/05-primeiro-projeto-react/src/routes/index.tsx
@@ -8,7 +8,7 @@ const Routes: React.FC = () => (
   <Switch>
     {/* Garante que apenas uma rota seja exibida */}
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
