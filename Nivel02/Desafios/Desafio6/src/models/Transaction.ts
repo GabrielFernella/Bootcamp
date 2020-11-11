3d67fe8f2e240a754489c4a24d5c65f9f98a5ff6
GabrielDEV <gabrielfernella07@hotmail.com>
2020-11-11 00:44:25 -0300

    models

--- Nivel02/Desafios/Desafio6/src/models/Transaction.ts
+++ Nivel02/Desafios/Desafio6/src/models/Transaction.ts
@@ -1,16 +1,44 @@
+/*
+  Esse arquivo é responsável por fazer a relação entre o model e os campos no
+  banco de dados, vc precisa atribuir os decorators para que o ORM possa identificar.
+
+  Repare que na parte de relacionamento é necessário que vc importe os decorators necessários.
+*/
+
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- Nivel02/Desafios/Desafio6/steps.md
+++ Nivel02/Desafios/Desafio6/steps.md
@@ -2,6 +2,9 @@
 
 ## Processos
 
-1. Rode a seguinte migration => yarn typeorm migration:create -n CreateTransactions && yarn typeorm migration:create -n CreateCategories
+1. Rode a seguinte migration => yarn typeorm migration:create -n CreateTransactions
+2. yarn typeorm migration:create -n CreateCategories
+3. yarn typeorm migration:create -n AddCategoryIdToTransactions
+4. yarn typeorm migration:run || revert
 
 ## Anotações
