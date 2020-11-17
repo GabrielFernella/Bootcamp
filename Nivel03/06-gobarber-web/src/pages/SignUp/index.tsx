d373ca23f6c65c9ce4a208976a5c4baa56b433d5
Gabriel Fernella <39594204+GabrielFernella@users.noreply.github.com>
2020-11-17 14:36:21 -0300

    Ferramenta para manipulação de erros

--- Nivel03/06-gobarber-web/src/pages/SignUp/index.tsx
+++ Nivel03/06-gobarber-web/src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logo from '../../assets/logo.svg';
 
@@ -12,8 +14,13 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  // Validando os campos do Imput
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({}); //Precisa iniciar vazio para resetar
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -26,7 +33,9 @@ const SignUp: React.FC = () => {
         abortEarly: false, // permite que vc retorne todos os erros de uma vez só
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -38,7 +47,7 @@ const SignUp: React.FC = () => {
         <Content>
           <img src={logo} alt="GoBarber" />
 
-          <Form onSubmit={handleSubmit}>
+          <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu Cadastro</h1>
             <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
             <Input
