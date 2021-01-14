05ea53079ed9c62954dbfe1792f951796bb03c66
GabrielDEV <gabrielfernella07@hotmail.com>
2021-01-13 21:03:58 -0300

    Começando com TDD

---
+++ Nivel05/Projeto-Node/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+
+//import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail'); //Guardando uma referencia dessa classe para ver se a mesma foi executada
+
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmailService.execute({
+      email: 'johndoe@exemple.com',
+    });
+
+    // espera que user tenha um id
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
