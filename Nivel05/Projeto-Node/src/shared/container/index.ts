2940dfa1fb1af746fad67bf43921413a5137b8bb
Gabriel Fernella <GabrielFernella@github.com>
2021-01-15 09:00:20 -0300

    Created Routes and repositories reset and forgot password

--- Nivel05/Projeto-Node/src/shared/container/index.ts
+++ Nivel05/Projeto-Node/src/shared/container/index.ts
@@ -1,3 +1,5 @@
+// Esse arquivo é responsável por fazer as importações de injeção de dependência
+
 import { container } from 'tsyringe';
 
 import '@models/users/providers'; // alterado no eslint
@@ -9,13 +11,21 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 // Utilizamos o singleton que instancie nossa classe uma única vez durante todo o siclo de vida da aplicação
-container.registerSingleton<IAppointmentsRepository>(
-  'AppointmentsRepository',
-  AppointmentsRepository,
+container.registerSingleton<IAppointmentsRepository>( // A interface utilizada para guia
+  'AppointmentsRepository', // Como deve ser chamada o inject
+  AppointmentsRepository, // O respectivo repositório de referencia
 );
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
