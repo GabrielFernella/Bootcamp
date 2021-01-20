4356571c11865e4d3a50b8bce0f21397892d0a70
Gabriel Fernella <GabrielFernella@github.com>
2021-01-20 12:23:18 -0300

    List appointments Day

--- Nivel05/Projeto-Node/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ Nivel05/Projeto-Node/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,12 +1,19 @@
 // Esse arquivo funciona como uma interface para que quando utilizar esses mpetodos em serviços, teremos que respeitar os métodos da interface
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
+
   findByDate(date: Date): Promise<Appointment | undefined>;
+
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
+
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO,
   ): Promise<Appointment[]>;
 }
